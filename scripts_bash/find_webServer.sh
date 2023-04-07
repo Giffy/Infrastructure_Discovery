9c63e7a60c946895fdb689475af4093fe711a859
Giffy <1595343+Giffy@users.noreply.github.com>
2023-04-08 01:01:39 +0200

    Added find_webserver script in bash

--- scripts_bash/find_webServer.sh
+++ scripts_bash/find_webServer.sh
@@ -1,15 +1,24 @@
-
+#!/bin/bash
 # Importing modules
 source ../modules/error_management.sh
 source ../modules/read_setup.sh
 
 # Read setup files
-#read_setup_ini_file
+#echo "version: "$(get_setting_value "Version" "version_num" )
 
-echo "version: "$(get_setting_value "Version" "version_num" )
+IP_PREFIX="192.168.1."
+PORT_RANGE="80 443 8080 8443"
 
-# curl -I -silent http://www.google.com | grep Server
-
-
-#install.sh
+# Loop over each port in the range
+for PORT in ${PORT_RANGE}; do
+  # Loop over each IP address in the range
+  for IP in $(seq -f ${IP_PREFIX}"%g" 1 254); do
+    # Make a curl request to Web servers
+    result=$(curl --max-time 3 --silent --head http://${IP}:${PORT} | grep "Server")
+    # Check if curl succeeded and Web Server was found
+    if [ $? -eq 0 ]; then
+      echo "Web server is running on ${IP}:${PORT} ${result}"
+    fi
+  done
+done
 
